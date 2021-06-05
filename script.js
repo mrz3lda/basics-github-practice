beab05a2bedbc63bff662e669083ba18714b1e91
mrz3lda <yeow.hua.ong@gmail.com>
2021-06-05 13:53:24 +0800

    secret word Mary YH

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
+var noOfWins = 0;
+
+// dice roll
+var diceRoll = function () {
+  var randomDecimal = Math.random() * 3;
+  var randomInteger = Math.floor(randomDecimal);
+  var diceNumber = randomInteger + 1;
+  return diceNumber;
+};
+
+// assign dice number to secret words
+var generateSecretWord = function (randomDiceRoll) {
+  var randomDiceRoll = diceRoll();
+  console.log(randomDiceRoll);
+  if (randomDiceRoll == 1) {
+    return 'banana';
+  }
+  if (randomDiceRoll == 2) {
+    return 'chisel';
+  }
+  return 'faucet';
+};
+
 var main = function (input) {
-  var myOutputValue = 'hello world';
+  var secretWord = generateSecretWord();
+  console.log(secretWord);
+  var noOfWinsLeft = 2 - noOfWins;
+  var myOutputValue =
+    'The guessed word is ' +
+    input +
+    ' and the secret word is ' +
+    secretWord +
+    '. You need ' +
+    noOfWinsLeft +
+    ' to win.';
+  if (input == secretWord) {
+    noOfWins = noOfWins + 1;
+    noOfWinsLeft = 2 - noOfWins;
+    var myOutputValue =
+      'The guessed word is ' +
+      input +
+      ' and the secret word is ' +
+      secretWord +
+      '. You need ' +
+      noOfWinsLeft +
+      ' to win.';
+  }
   return myOutputValue;
 };
